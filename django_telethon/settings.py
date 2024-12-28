f424c9f3d93597c9167267069a038c09a0674244
yanakozarenko2022 <yanakozarenko2024@gmail.com>
2024-12-28 12:01:22 +0000

    add decouple

--- django_telethon/settings.py
+++ django_telethon/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
